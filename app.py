4e39824d72884503ef77cc4901aed38bb0d04ee7
creaked <creaked@gmail.com>
2020-11-12 03:59:14 -0500

    fix CORS issue

--- app.py
+++ app.py
@@ -8,18 +8,19 @@ from sqlalchemy.sql import expression
 # Init app
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 # Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
     os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 # Init db
 db = SQLAlchemy(app)
+
 # Init ma
 ma = Marshmallow(app)
 
 # Todo Class/Model
-
-
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), unique=False)
@@ -31,20 +32,15 @@ class Todo(db.Model):
         self.completed = completed
 
 # Todo Schema
-
-
 class TodoSchema(ma.Schema):
     class Meta:
         fields = ('id', 'title', 'completed')
-
 
 # Init Schema
 todo_schema = TodoSchema()
 todos_schema = TodoSchema(many=True)
 
 # Create Todo
-
-
 @app.route('/todo', methods=['POST'])
 def add_todo():
     title = request.json['title']
@@ -57,14 +53,11 @@ def add_todo():
     return todo_schema.jsonify(new_todo)
 
 # Get All Todos
-
-
 @app.route('/todo', methods=['GET'])
 def get_todos():
     all_todos = Todo.query.all()
     result = todos_schema.dump(all_todos)
     return jsonify(result)
-
 
 # Get Todo
 @app.route('/todo/<id>', methods=['GET'])
@@ -73,8 +66,6 @@ def get_todo(id):
     return todo_schema.jsonify(todo)
 
 # Update Todo
-
-
 @app.route('/todo/<id>', methods=['PUT'])
 def update_todo(id):
     todo = Todo.query.get(id)
@@ -96,6 +87,15 @@ def delete_todo(id):
     db.session.delete(todo)
     db.session.commit()
     return todo_schema.jsonify(todo)
+
+
+@app.after_request
+def after_request(response):
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    header['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    header['Access-Control-Allow-Methods'] = 'OPTIONS, HEAD, GET, POST, DELETE, PUT'
+    return response
 
 
 # Run Server
